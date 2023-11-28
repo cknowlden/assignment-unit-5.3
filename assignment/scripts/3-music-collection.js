83aaf596140399c9040824a0c476da000103bd76
Cali Knowlden <calbearnc@gmail.com>
2023-11-28 15:39:57 -0600

    fixed the addtocollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,37 +3,26 @@ console.log('***** Music Collection *****')
 
 let myCollection = [];
 
-console.log('my collection includes', this.myCollection);
-
-// {
-//   title: 'Rumours',
-//   artist: 'Fleetwood Mac',
-//   yearPublished: 1977,
-//  },
-
-
 function addToCollection(collection, title, artist, yearPublished) {
-  console.log('in collection function');
+  //console.log('in collection function');
   let album = {
-    collection: collection,
     title: title,
     artist: artist,
     yearPublished: yearPublished,
   };
-  myCollection.push(album);
-  //collection.push(album);
+  collection.push(album);
   return album;
 };
-console.log(`1 my new collection has ${addToCollection('classicRock', 'Rumours', 'Fleetwood Mac', 1977)}`)
-console.log(`1 my new collection has ${addToCollection('classicRock', 'Candy-O', 'The Cars', 1979)}`)
-console.log(`1 my new collection has ${addToCollection('hipHop', 'Very Necessary', 'Salt-N-Pepa', 1993)}`)
-console.log(`1 my new collection has ${addToCollection('emo', 'Tell All Your Friends', 'Taking Back Sunday', 2002)}`)
-console.log(`1 my new collection has ${addToCollection('classicRock', 'Moondance', 'Van Morrison', 1970)}`)
-console.log(`1 my new collection has ${addToCollection('emo', 'Let It Enfold You', 'Senses Fail', 2004)}`)
-
-myCollection.push(addToCollection);
+addToCollection(myCollection, 'Rumours', 'Fleetwood Mac', 1977);
+addToCollection(myCollection, 'Candy-O', 'The Cars', 1979);
+addToCollection(myCollection, 'Very Necessary', 'Salt-N-Pepa', 1993);
+addToCollection(myCollection, 'Tell All Your Friends', 'Taking Back Sunday', 2002);
+addToCollection(myCollection, 'Moondance', 'Van Morrison', 1970);
+addToCollection(myCollection, 'Let It Enfold You', 'Senses Fail', 2004);
 console.log(myCollection);
 
+//loop through collection
+//for (album of )
 
 
 
